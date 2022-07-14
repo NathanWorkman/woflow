0bc7effe135b3714a25a90191e78d871676f1940
Nathan Workman <nathancworkman@gmail.com>
2022-07-14 12:44:54 -0400

    Comments and setup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Woflow
+To setup you'll need python 3.6 or > 
+## Setup
+
+- `git clone git@github.com:NathanWorkman/woflow.git`
+- `cd woflow`
+- `pip install -r requirements.txt`
+
+## Run
+
+- `python main.py`
+
+## Results
+
+1. 30 unique nodes
+2. `a06c90bf-e635-4812-992e-f7b1e2408a3f` most common id
--- main.py
+++ main.py
@@ -5,25 +5,31 @@ unique_ids = set()
 
 
 def main():
+    # Set start ID
     search_id = f"089ef556-dfff-4ff2-9733-654645be56fe"
     get_ids(search_id)
-    most_frequent(all_ids)
     print(len(unique_ids))
+    print(most_frequent(all_ids))
 
 
 def get_ids(search_id):
+    # Set list of id's found in each node and child node
     ids = []
+    # Set list of all ids
     all_ids.append(search_id)
+    # Create set of unique id's
     unique_ids.add(search_id)
     r = requests.get(
         f"https://nodes-on-nodes-challenge.herokuapp.com/nodes/{search_id}"
     )
     response = r.json()
 
+    # Traverse each response
     for obj in response:
         if isinstance(obj, dict):
             ids.append(obj.get("id"))
             children = obj.get("child_node_ids", None)
+            # If child nodes are present traverse array
             if children:
                 for item in children:
                     ids.append(item)
@@ -38,7 +44,6 @@ def most_frequent(id_list):
         dict[item] = dict.get(item, 0) + 1
         if dict[item] >= count:
             count, itm = dict[item], item
-    print(itm)
     return itm
 
 
